c64baf7fd9f43f02905097cc31eb99251439fce2
slonegd <slonkdv@mail.ru>
2018-10-28 13:24:39 +0300

    v3.014 f0_rcc for boost test and travis

--- test/f0_boosttest_rcc.cpp
+++ test/f0_boosttest_rcc.cpp
@@ -26,7 +26,6 @@ BOOST_AUTO_TEST_CASE (make_reference)
 BOOST_AUTO_TEST_CASE (set_AHBprescaler)
 {
    CMSIS.CFGR = 0;
-   bool good {true};
    rcc.set (mcu::RCC::AHBprescaler::AHBdiv512);
    BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_HPRE_0), true);
    BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_HPRE_1), true);
@@ -79,171 +78,130 @@ BOOST_AUTO_TEST_CASE (set_AHBprescaler)
    BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_HPRE_3), false);
 }
 
+BOOST_AUTO_TEST_CASE (set_APBprescaler)
+{
+   CMSIS.CFGR = 0;
+   rcc.set (mcu::RCC::APBprescaler::APBdiv16);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_0), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_1), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_2), true);
+
+   rcc.set (mcu::RCC::APBprescaler::APBdiv8);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_0), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_1), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_2), true);
+
+   rcc.set (mcu::RCC::APBprescaler::APBdiv4);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_0), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_1), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_2), true);
+
+   rcc.set (mcu::RCC::APBprescaler::APBdiv2);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_0), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_1), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_2), true);
+
+   rcc.set (mcu::RCC::APBprescaler::APBnotdiv);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PPRE_2), false);
+}
+
+BOOST_AUTO_TEST_CASE (set_SystemClock)
+{
+   CMSIS.CFGR = 0;
+   rcc.set (mcu::RCC::SystemClock::CS_PLL);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_SW_0), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_SW_1), true);
+
+   rcc.set (mcu::RCC::SystemClock::CS_HSE);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_SW_0), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_SW_1), false);
+
+   rcc.set (mcu::RCC::SystemClock::CS_HSI);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_SW_0), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_SW_1), false);
+}
+
+BOOST_AUTO_TEST_CASE (set_PLLsource)
+{
+   CMSIS.CFGR = 0;
+   rcc.set (mcu::RCC::PLLsource::HSE);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLSRC_Msk), true);
+
+   rcc.set (mcu::RCC::PLLsource::HSIdiv2);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLSRC_Msk), false);
+}
+
+BOOST_AUTO_TEST_CASE (set_PLLmultiplier)
+{
+   CMSIS.CFGR = 0;
+   rcc.set (mcu::RCC::PLLmultiplier::_16);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_1), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_2), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_3), true);
+
+   rcc.set (mcu::RCC::PLLmultiplier::_15);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_0), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_1), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_2), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_3), true);
+
+   rcc.set (mcu::RCC::PLLmultiplier::_10);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_0), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_1), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_2), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_3), true);
+
+   rcc.set (mcu::RCC::PLLmultiplier::_9);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_0), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_1), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_2), true);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_3), false);
+
+   rcc.set (mcu::RCC::PLLmultiplier::_2);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_0), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_1), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_2), false);
+   BOOST_CHECK_EQUAL (bool(CMSIS.CFGR & RCC_CFGR_PLLMUL_3), false);
+}
+
+BOOST_AUTO_TEST_CASE (on_PLL)
+{
+   CMSIS.CR = 0;
+   rcc.on_PLL();
+   BOOST_CHECK_EQUAL (bool(CMSIS.CR & RCC_CR_PLLON_Msk), true);
+}
+
+BOOST_AUTO_TEST_CASE (wait_PLL_ready)
+{
+   // TODO: timeout when failed
+   CMSIS.CR = 0;
+   bool good {true};
+   bool work {true};
+   auto worker = [&]() { rcc.wait_PLL_ready(); work = false; };
+   std::thread {worker}.detach();
+   std::this_thread::sleep_for(std::chrono::milliseconds(100));
+   good &= work;
+   CMSIS.CR |= RCC_CR_PLLRDY_Msk;
+   while (work) {}
+}
+
+BOOST_AUTO_TEST_CASE (clock_enable)
+{
+   CMSIS.AHBENR = 0;
+   rcc.clock_enable<mcu::Periph::GPIOA>();
+   BOOST_CHECK_EQUAL (bool(CMSIS.AHBENR & RCC_AHBENR_GPIOAEN_Msk), true);
+
+   rcc.clock_enable<mcu::Periph::GPIOB>();
+   BOOST_CHECK_EQUAL (bool(CMSIS.AHBENR & RCC_AHBENR_GPIOBEN_Msk), true);
+
+   rcc.clock_enable<mcu::Periph::GPIOC>();
+   BOOST_CHECK_EQUAL (bool(CMSIS.AHBENR & RCC_AHBENR_GPIOCEN_Msk), true);
+
+   rcc.clock_enable<mcu::Periph::GPIOD>();
+   BOOST_CHECK_EQUAL (bool(CMSIS.AHBENR & RCC_AHBENR_GPIODEN_Msk), true);
+
+   rcc.clock_enable<mcu::Periph::GPIOF>();
+   BOOST_CHECK_EQUAL (bool(CMSIS.AHBENR & RCC_AHBENR_GPIOFEN_Msk), true);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
-
-
-
-
-// bool set_APBprescaler()
-// {
-//    CMSIS.CFGR = 0;
-//    bool good {true};
-//    rcc.set (mcu::RCC::APBprescaler::APBdiv16);
-//    good &=     (CMSIS.CFGR & RCC_CFGR_PPRE_0)
-//        and     (CMSIS.CFGR & RCC_CFGR_PPRE_1)
-//        and     (CMSIS.CFGR & RCC_CFGR_PPRE_2);
-
-//    rcc.set (mcu::RCC::APBprescaler::APBdiv8);
-//    good &= not (CMSIS.CFGR & RCC_CFGR_PPRE_0)
-//        and     (CMSIS.CFGR & RCC_CFGR_PPRE_1)
-//        and     (CMSIS.CFGR & RCC_CFGR_PPRE_2);
-
-//    rcc.set (mcu::RCC::APBprescaler::APBdiv4);
-//    good &=     (CMSIS.CFGR & RCC_CFGR_PPRE_0)
-//        and not (CMSIS.CFGR & RCC_CFGR_PPRE_1)
-//        and     (CMSIS.CFGR & RCC_CFGR_PPRE_2);
-
-//    rcc.set (mcu::RCC::APBprescaler::APBdiv2);
-//    good &= not (CMSIS.CFGR & RCC_CFGR_PPRE_0)
-//        and not (CMSIS.CFGR & RCC_CFGR_PPRE_1)
-//        and     (CMSIS.CFGR & RCC_CFGR_PPRE_2);
-
-//    rcc.set (mcu::RCC::APBprescaler::APBnotdiv);
-//    good &= not (CMSIS.CFGR & RCC_CFGR_PPRE_2);
-
-//    return good;
-// }
-
-// bool set_SystemClock()
-// {
-//    CMSIS.CFGR = 0;
-//    bool good {true};
-//    rcc.set (mcu::RCC::SystemClock::CS_PLL);
-//    good &= not (CMSIS.CFGR & RCC_CFGR_SW_0)
-//        and     (CMSIS.CFGR & RCC_CFGR_SW_1);
-
-//    rcc.set (mcu::RCC::SystemClock::CS_HSE);
-//    good &=     (CMSIS.CFGR & RCC_CFGR_SW_0)
-//        and not (CMSIS.CFGR & RCC_CFGR_SW_1);
-
-//    rcc.set (mcu::RCC::SystemClock::CS_HSI);
-//    good &= not (CMSIS.CFGR & RCC_CFGR_SW_0)
-//        and not (CMSIS.CFGR & RCC_CFGR_SW_1);
-
-//    return good;
-// }
-
-// bool set_PLLsource()
-// {
-//    CMSIS.CFGR = 0;
-//    bool good {true};
-//    rcc.set (mcu::RCC::PLLsource::HSE);
-//    good &=     bool(CMSIS.CFGR & RCC_CFGR_PLLSRC_Msk);
-
-//    rcc.set (mcu::RCC::PLLsource::HSIdiv2);
-//    good &= not bool(CMSIS.CFGR & RCC_CFGR_PLLSRC_Msk);
-
-//    return good;
-// }
-
-// bool set_PLLmultiplier()
-// {
-//    CMSIS.CFGR = 0;
-//    bool good {true};
-//    rcc.set (mcu::RCC::PLLmultiplier::_16);
-//    good &=     (CMSIS.CFGR & RCC_CFGR_PLLMUL_1)
-//        and     (CMSIS.CFGR & RCC_CFGR_PLLMUL_2)
-//        and     (CMSIS.CFGR & RCC_CFGR_PLLMUL_3);
-
-//    rcc.set (mcu::RCC::PLLmultiplier::_15);
-//    good &=     (CMSIS.CFGR & RCC_CFGR_PLLMUL_0)
-//        and not (CMSIS.CFGR & RCC_CFGR_PLLMUL_1)
-//        and     (CMSIS.CFGR & RCC_CFGR_PLLMUL_2)
-//        and     (CMSIS.CFGR & RCC_CFGR_PLLMUL_3);
-
-//    rcc.set (mcu::RCC::PLLmultiplier::_10);
-//    good &= not (CMSIS.CFGR & RCC_CFGR_PLLMUL_0)
-//        and not (CMSIS.CFGR & RCC_CFGR_PLLMUL_1)
-//        and not (CMSIS.CFGR & RCC_CFGR_PLLMUL_2)
-//        and     (CMSIS.CFGR & RCC_CFGR_PLLMUL_3);
-
-//    rcc.set (mcu::RCC::PLLmultiplier::_9);
-//    good &=     (CMSIS.CFGR & RCC_CFGR_PLLMUL_0)
-//        and     (CMSIS.CFGR & RCC_CFGR_PLLMUL_1)
-//        and     (CMSIS.CFGR & RCC_CFGR_PLLMUL_2)
-//        and not (CMSIS.CFGR & RCC_CFGR_PLLMUL_3);
-
-//    rcc.set (mcu::RCC::PLLmultiplier::_2);
-//    good &= not (CMSIS.CFGR & RCC_CFGR_PLLMUL_0)
-//        and not (CMSIS.CFGR & RCC_CFGR_PLLMUL_1)
-//        and not (CMSIS.CFGR & RCC_CFGR_PLLMUL_2)
-//        and not (CMSIS.CFGR & RCC_CFGR_PLLMUL_3);
-
-
-//    return good;
-// }
-
-// bool on_PLL()
-// {
-//    CMSIS.CR = 0;
-//    rcc.on_PLL();
-//    return CMSIS.CR & RCC_CR_PLLON_Msk;
-// }
-
-// bool wait_PLL_ready()
-// {
-//    CMSIS.CR = 0;
-//    bool good {true};
-//    bool work {true};
-//    auto worker = [&]() { rcc.wait_PLL_ready(); work = false; };
-//    std::thread {worker}.detach();
-//    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//    good &= work;
-//    CMSIS.CR |= RCC_CR_PLLRDY_Msk;
-//    while (work) {}
-
-//    return good;
-// }
-
-// bool clock_enable()
-// {
-//    CMSIS.AHBENR = 0;
-//    bool good {true};
-
-//    rcc.clock_enable<mcu::Periph::GPIOA>();
-//    good &= bool(CMSIS.AHBENR & RCC_AHBENR_GPIOAEN_Msk);
-
-//    rcc.clock_enable<mcu::Periph::GPIOB>();
-//    good &= bool(CMSIS.AHBENR & RCC_AHBENR_GPIOBEN_Msk);
-
-//    rcc.clock_enable<mcu::Periph::GPIOC>();
-//    good &= bool(CMSIS.AHBENR & RCC_AHBENR_GPIOCEN_Msk);
-
-//    rcc.clock_enable<mcu::Periph::GPIOD>();
-//    good &= bool(CMSIS.AHBENR & RCC_AHBENR_GPIODEN_Msk);
-
-//    rcc.clock_enable<mcu::Periph::GPIOF>();
-//    good &= bool(CMSIS.AHBENR & RCC_AHBENR_GPIOFEN_Msk);
-
-//    return good;
-// }
-
-// int main()
-// {
-//    std::cout << '\n'
-//              << "Тесты класса RCC для STM32F0:" << std::endl;
-
-//    auto test = [](auto s, auto f){
-//       std::cout << s << (f() ? "\033[32mпрошёл\033[0m" : "\033[31mпровален\033[0m") << std::endl;
-//    };
-
-//    test ("RCC::make_reference        ", make_reference);
-//    test ("RCC::set_AHBprescaler      ", set_AHBprescaler);
-//    test ("RCC::set_APBprescaler      ", set_APBprescaler);
-//    test ("RCC::set_SystemClock       ", set_SystemClock);
-//    test ("RCC::set_PLLsource         ", set_PLLsource);
-//    test ("RCC::set_PLLmultiplier     ", set_PLLmultiplier);
-//    test ("RCC::on_PLL                ", on_PLL);
-//    test ("RCC::wait_PLL_ready        ", wait_PLL_ready);
-//    test ("RCC::clock_enable          ", clock_enable);
-// }
