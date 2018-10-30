c1847b54eee2ff586ea523f68a5af426d4f01d83
Денис Владимирович Кирюшин <dvk@phobos.factory.alexplus.ru>
2018-10-30 13:40:21 +0300

    f4 flash boost test

--- src/periph/f4_flash.h
+++ src/periph/f4_flash.h
@@ -51,7 +51,7 @@ FLASH& FLASH::unlock()
    constexpr uint32_t Key2 = 0xCDEF89AB;
    if (is_lock()) {
       KEYR = Key1;
-      IF_TEST_WAIT_MS(10);
+      IF_TEST_WAIT_MS(1);
       KEYR = Key2;
    }
    return *this;
