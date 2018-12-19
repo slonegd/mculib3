b96dbe91bf52ac2f4c42cf678250363f9a38c434
Денис Владимирович Кирюшин <dvk@phobos.factory.alexplus.ru>
2018-12-19 11:55:24 +0300

    add f0_buts_flash from mculib2

---
+++ src/bits/f0_bits_flash.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <cstdint>
+
+namespace mcu::FLASH_bits {
+
+struct ACR {
+   enum Latency { _0 = 0, _1 };
+   Latency  LATENCY :3; // Bits 2:0 LATENCY[2:0]: Latency
+   uint32_t         :1; // Bit 3 Reserved, must be kept at reset value.
+   bool     PRFTBE  :1; // Bit 4 PRFTBE: Prefetch buffer enable
+   bool     PRFTBS  :1; // Bit 5 PRFTBS: Prefetch buffer status
+   uint32_t         :26; // Bits 31:6 Reserved, must be kept at reset value.
+}__attribute__((packed));
+
+struct SR {
+   bool     BSY      :1; // Bit 0 BSY: Busy
+   uint32_t          :1; // Bit 1 Reserved, must be kept at reset value
+   bool     PGERR    :1; // Bit 2 PGERR: Programming error
+   uint32_t          :1; // Bit 3 Reserved, must be kept at reset value.
+   bool     WRPRTERR :1; // Bit 4 WRPRTERR: Write protection error
+   bool     EOP      :1; // Bit 5 EOP: End of operation
+   uint32_t          :26;
+}__attribute__((packed));
+
+struct CR {
+   bool     PG         :1; // Bit 0 PG: Programming
+   bool     PER        :1; // Bit 1 SER: Sector Erase
+   bool     MER        :1; // Bit 2 MER: Mass Erase
+   uint32_t            :1; // Bit 3 Reserved, must be kept at reset value.
+   bool     OPTPG      :1; // Bit 4 OPTPG: Option byte programming
+   bool     OPTER      :1; // Bit 5 OPTER: Option byte erase
+   bool     STRT       :1; // Bit 6 STRT: Start
+   bool     LOCK       :1; // Bit 7 LOCK: Lock
+   uint32_t            :1; // Bit 8 Reserved, must be kept at reset value.
+   bool     OPTWRE     :1; // Bit 9 OPTWRE: Option byte write enable
+   bool     ERRIE      :1; // Bit 10 ERRIE: Error interrupt enable
+   uint32_t            :1; // Bit 11 Reserved, must be kept at reset value
+   bool     EOPIE      :1; // Bit 12 EOPIE: End of operation interrupt enable
+   bool     OBL_LAUNCH :1; // Bit 13 OBL_LAUNCH: Force option byte loading
+   uint32_t            :16; // Bits 31:14 Reserved, must be kept at reset value.
+}__attribute__((packed));
+
+} // namespace mcu::FLASH_bits {
+
