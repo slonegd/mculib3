21b20218d9d324015a3bacf27df92c76934f8d6d
Денис Владимирович Кирюшин <dvk@phobos.factory.alexplus.ru>
2018-12-26 08:56:23 +0300

    some meta decor

--- src/meta.h
+++ src/meta.h
@@ -8,28 +8,7 @@
 #include <array>
 
 
-// определяет количество типов вариадика, пример:
-// const uint8_t qty = QtyTypes<Types...>::value;
-template <class T, class ... Types> struct QtyTypes {
-   static const uint8_t value = 1 + QtyTypes<Types...>::value;
-};
-template<class T> struct QtyTypes<T> {
-   static const uint8_t value = 1;
-};
-
-
-
-// определяет конкретный тип аргумента вариадика, пример:
-// using Type = At<2, Types...>::Result;
-template<size_t n, class T, class ... Types> struct At {
-   using Result = typename At<n-1, Types...>::Result;
-};
-template<class T, class ... Types> struct At<1,T,Types...> {
-   using Result = T;
-};
-
-template <size_t n, class ... Ts>
-using at_t = typename At<n,Ts...>::Result;
+namespace meta {
 
 // определяет первое положение конкретного типа из вариадика, пример:
 // начиная с 1
@@ -58,24 +37,31 @@ constexpr int position_v = Position<T,Ts...>::value;
 
 
 /// генерирация массива данных
-template<auto f, class index_sequence>
+template<auto& f, class index_sequence>
 struct generate_impl;
 
-template<auto f, size_t ... i>
+template<auto& f, size_t ... i>
 struct generate_impl<f, std::index_sequence<i...>>
 {
    using type = decltype(f(0));
-   static constexpr std::array<type,sizeof...(i)> table {f(i)...};
+   static constexpr auto size = sizeof...(i);
+   static constexpr std::array<type,size> table {f(i)...};
 };
 
 /// генерировать массив функцией f
-template<auto f, size_t size>
+template<auto& f, size_t size>
 constexpr auto generate = generate_impl<f, std::make_index_sequence<size>>::table;
+
+} // namespace meta {
+
+namespace { // test
 
 /// пример генерирующей функции
 constexpr size_t fibo (size_t i) { return (i > 1) ? fibo(i-2) + fibo(i-1) : 1; }
+
 /// пример использования + тест
-constexpr auto fibo_example = generate<fibo, 6>;
+constexpr auto fibo_example = meta::generate<fibo, 6>;
+
 static_assert (fibo_example[0] == 1);
 static_assert (fibo_example[1] == 1);
 static_assert (fibo_example[2] == 2);
@@ -83,5 +69,7 @@ static_assert (fibo_example[3] == 3);
 static_assert (fibo_example[4] == 5);
 static_assert (fibo_example[5] == 8);
 static_assert (fibo_example.size() == 6);
+
+} // namespace {
 
 
