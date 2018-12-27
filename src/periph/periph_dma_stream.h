da9acf7ac9c308bb9110258b3c70ee9c5d9c862f
Денис Владимирович Кирюшин <dvk@phobos.factory.alexplus.ru>
2018-12-27 10:02:06 +0300

    expand f1 dma stream bits to f0_f1

---
+++ src/periph/periph_dma_stream.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "periph.h"
+
+#if defined(STM32F0)
+   #include "f0_dma_stream.h"
+// #elif defined(STM32F1)
+//    #include "f1_dma_stream.h"
+// #elif defined(STM32F4) or defined(STM32F7)
+//    #include "f4_f7_dma.h"
+#endif
