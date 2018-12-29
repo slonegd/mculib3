c59856b258b8faad5618687a47766320f4193404
Денис Владимирович Кирюшин <dvk@phobos.factory.alexplus.ru>
2018-12-29 17:05:07 +0300

    to home

--- src/periph/f1_usart.h
+++ src/periph/f1_usart.h
@@ -2,30 +2,33 @@
 
 #include <type_traits>
 #include "f1_bits_usart.h"
-#include "rcc.h"
+#include "periph_rcc.h"
 #include "pin.h"
-#include "dma.h"
+#include "periph_dma.h"
 
 namespace mcu {
 
-class USART_default
-{
-public:
-   __IO USART_bits::SR  SR;   // USART Status register,                   offset: 0x00
-   __IO uint32_t        DR;   // USART Data register,                     offset: 0x04
-   __IO uint32_t        BRR;  // USART Baud rate register,                offset: 0x08
-   __IO USART_bits::CR1 CR1;  // USART Control register 1,                offset: 0x0C
-   __IO USART_bits::CR2 CR2;  // USART Control register 2,                offset: 0x10
-   __IO USART_bits::CR3 CR3;  // USART Control register 3,                offset: 0x14
-   __IO uint32_t        GTPR; // USART Guard time and prescaler register, offset: 0x18
-   // this thing for test
-   auto read_SR() { return *reinterpret_cast<__IO uint32_t*>(&SR); }
+struct USART_data {
+   volatile USART_bits::SR  SR;   // USART Status register,                   offset: 0x00
+   volatile uint32_t        DR;   // USART Data register,                     offset: 0x04
+   volatile uint32_t        BRR;  // USART Baud rate register,                offset: 0x08
+   volatile USART_bits::CR1 CR1;  // USART Control register 1,                offset: 0x0C
+   volatile USART_bits::CR2 CR2;  // USART Control register 2,                offset: 0x10
+   volatile USART_bits::CR3 CR3;  // USART Control register 3,                offset: 0x14
+   volatile uint32_t        GTPR; // USART Guard time and prescaler register, offset: 0x18
+ 
 };
 
-template<class R = USART_default>
-class USART_ : private R {
+class USART : protected USART_data {
+// protected:
+//    volatile USART_bits::SR  SR;   // USART Status register,                   offset: 0x00
+//    volatile uint32_t        DR;   // USART Data register,                     offset: 0x04
+//    volatile uint32_t        BRR;  // USART Baud rate register,                offset: 0x08
+//    volatile USART_bits::CR1 CR1;  // USART Control register 1,                offset: 0x0C
+//    volatile USART_bits::CR2 CR2;  // USART Control register 2,                offset: 0x10
+//    volatile USART_bits::CR3 CR3;  // USART Control register 3,                offset: 0x14
+//    volatile uint32_t        GTPR; // USART Guard time and prescaler register, offset: 0x18
 public:
-
    using CMSIS_type     = USART_TypeDef;
    using Parity         = USART_bits::CR1::Parity;
    using WakeMethod     = USART_bits::CR1::WakeMethod;
@@ -44,44 +47,50 @@ public:
       BR115200 = 0b111  // modbus time 2 ms
    };
 
-   USART_& set (Parity         v)         {this->CR1.PS    = v; return *this;}
-   USART_& set (WakeMethod     v)         {this->CR1.WAKE  = v; return *this;}
-   USART_& set (DataBits       v)         {this->CR1.M     = v; return *this;}
-   USART_& set (BreakDetection v)         {this->CR2.LBDL  = v; return *this;}
-   USART_& set (StopBits       v)         {this->CR2.STOP  = v; return *this;}
-   USART_& send_byte (uint8_t  v)         {this->DR = v;        return *this;}
-   USART_& set (Baudrate, size_t);
+   auto& like_CMSIS() { return *reinterpret_cast<CMSIS_type*>(static_cast<USART_data*>(this)); }
 
-   template <Periph usart, Periph rcc = Periph::RCC>
-   USART_& clock_enable  (){make_reference<rcc>().template clock_enable<usart>(); return *this;}
-   USART_& enable        (){this->CR1.UE   = true;  return *this;}
-   USART_& disable       (){this->CR1.UE   = false; return *this;}
-   USART_& rx_enable     (){this->CR1.RE   = true;  return *this;}
-   USART_& rx_disable    (){this->CR1.RE   = false; return *this;}
-   USART_& tx_enable     (){this->CR1.TE   = true;  return *this;}
-   USART_& tx_disable    (){this->CR1.TE   = false; return *this;}
-   USART_& rts_enable    (){this->CR3.RTSE = true;  return *this;}
-   USART_& rts_disable   (){this->CR3.RTSE = false; return *this;}
-   USART_& DMA_tx_enable (){this->CR3.DMAT = true;  return *this;}
-   USART_& DMA_rx_enable (){this->CR3.DMAR = true;  return *this;}
-   USART_& parity_enable (){this->CR1.PCE  = true;  return *this;}
-   USART_& parity_disable(){this->CR1.PCE  = false; return *this;}
-   bool    is_ready_read (){return this->SR.RXNE;}
+   USART& set (Parity         v)         {CR1.PS    = v; return *this;}
+   USART& set (WakeMethod     v)         {CR1.WAKE  = v; return *this;}
+   USART& set (DataBits       v)         {CR1.M     = v; return *this;}
+   USART& set (BreakDetection v)         {CR2.LBDL  = v; return *this;}
+   USART& set (StopBits       v)         {CR2.STOP  = v; return *this;}
+   USART& send_byte (uint8_t  v)         {DR = v;        return *this;}
+   USART& set (Baudrate, size_t);
 
-   USART_& enable_IDLE_interrupt          (){this->CR1.IDLEIE = true;  return *this;}
-   bool    is_IDLE_interrupt              (){return this->SR.IDLE;}
-   USART_& enable_tx_complete_interrupt   (){this->CR1.TCIE   = true;  return *this;}
-   USART_& disable_tx_complete_interrupt  (){this->CR1.TCIE   = false; return *this;}
-   bool    is_tx_complete                 (){return this->SR.TC;}
-   bool    is_tx_complete_interrupt_enable(){return this->CR1.TCIE;}
-   USART_& clear_interrupt_flags          (){
-      [[maybe_unused]] uint32_t t1 = this->read_SR();
-      [[maybe_unused]] uint32_t t2 = this->DR;
+   USART& enable        (){CR1.UE   = true;  return *this;}
+   USART& disable       (){CR1.UE   = false; return *this;}
+   USART& rx_enable     (){CR1.RE   = true;  return *this;}
+   USART& rx_disable    (){CR1.RE   = false; return *this;}
+   USART& tx_enable     (){CR1.TE   = true;  return *this;}
+   USART& tx_disable    (){CR1.TE   = false; return *this;}
+   USART& rts_enable    (){CR3.RTSE = true;  return *this;}
+   USART& rts_disable   (){CR3.RTSE = false; return *this;}
+   USART& DMA_tx_enable (){CR3.DMAT = true;  return *this;}
+   USART& DMA_rx_enable (){CR3.DMAR = true;  return *this;}
+   USART& parity_enable (){CR1.PCE  = true;  return *this;}
+   USART& parity_disable(){CR1.PCE  = false; return *this;}
+   bool    is_ready_read (){return SR.RXNE;}
+
+   USART& enable_IDLE_interrupt          (){CR1.IDLEIE = true;  return *this;}
+   bool   is_IDLE_interrupt              (){return SR.IDLE;}
+   USART& enable_tx_complete_interrupt   (){CR1.TCIE   = true;  return *this;}
+   USART& disable_tx_complete_interrupt  (){CR1.TCIE   = false; return *this;}
+   bool   is_tx_complete                 (){return SR.TC;}
+   bool   is_tx_complete_interrupt_enable(){return CR1.TCIE;}
+
+   
+   virtual uint32_t read_SR() { return *reinterpret_cast<volatile uint32_t*>(&SR); }
+   virtual uint32_t read_DR() { return DR; }
+   USART& clear_interrupt_flags()
+   {
+      [[maybe_unused]] auto _ = read_SR();
+      IF_TEST_WAIT_MS(100);
+                            _ = read_DR();
       return *this;
    }
 
-   size_t receive_data_adr () {return reinterpret_cast<size_t>(&this->DR);}
-   size_t transmit_data_adr() {return reinterpret_cast<size_t>(&this->DR);}
+   size_t receive_data_adr () {return reinterpret_cast<size_t>(&DR);}
+   size_t transmit_data_adr() {return reinterpret_cast<size_t>(&DR);}
 
    
    static constexpr IRQn_Type IRQn(Periph);
@@ -91,13 +100,13 @@ public:
    template<Periph usart, Periph rcc = Periph::RCC> static size_t clock();
 };
 
-using USART = USART_<>;
 
 
-
+#if not defined(USE_MOCK_USART)
 template <Periph p> std::enable_if_t<p == Periph::USART1, USART&> make_reference() {return *reinterpret_cast<USART*>(USART1_BASE);}
 template <Periph p> std::enable_if_t<p == Periph::USART2, USART&> make_reference() {return *reinterpret_cast<USART*>(USART2_BASE);}
 template <Periph p> std::enable_if_t<p == Periph::USART3, USART&> make_reference() {return *reinterpret_cast<USART*>(USART3_BASE);}
+#endif
 
 
 
@@ -116,33 +125,30 @@ template <Periph p> std::enable_if_t<p == Periph::USART3, USART&> make_reference
 
 
 
-
-
-template <class R>
-USART_<R>& USART_<R>::set(Baudrate baudrate, size_t clock)
+USART& USART::set(Baudrate baudrate, size_t clock)
 {
    switch (baudrate) {
       case BR9600:
-         this->BRR = clock/9600;  return *this;
+         BRR = clock/9600;  return *this;
       case BR14400:
-         this->BRR = clock/14400; return *this;
+         BRR = clock/14400; return *this;
       case BR19200:
-         this->BRR = clock/19200; return *this;
+         BRR = clock/19200; return *this;
       case BR28800:
-         this->BRR = clock/28800; return *this;
+         BRR = clock/28800; return *this;
       case BR38400:
-         this->BRR = clock/38400; return *this;
+         BRR = clock/38400; return *this;
       case BR57600:
-         this->BRR = clock/57600; return *this;
+         BRR = clock/57600; return *this;
       case BR76800:
-         this->BRR = clock/76800; return *this;
+         BRR = clock/76800; return *this;
       case BR115200:
-         this->BRR = clock/115200;return *this;
+         BRR = clock/115200;return *this;
    }
 }
 
-template <class R>
-template <Periph usart, class Pin> constexpr PinMode USART_<R>::pin_mode()
+
+template <Periph usart, class Pin> constexpr PinMode USART::pin_mode()
 {
    if constexpr (usart == Periph::USART1) {
       if      constexpr (std::is_same_v<Pin, PA9>  or std::is_same_v<Pin, PB6>) return PinMode::USART1_TX; 
@@ -161,17 +167,17 @@ template <Periph usart, class Pin> constexpr PinMode USART_<R>::pin_mode()
    }
 }
 
-template <class R>
+
 template <Periph usart, Periph rcc>
-size_t USART_<R>::clock()
+size_t USART::clock()
 {
    if      constexpr (usart == Periph::USART1) return make_reference<rcc>().get_APB2_clock();
    else if constexpr (usart == Periph::USART2) return make_reference<rcc>().get_APB1_clock();
    else if constexpr (usart == Periph::USART3) return make_reference<rcc>().get_APB1_clock();
 }
 
-template <class R>
-template<Periph usart, class Pin> constexpr Periph USART_<R>::stream()
+
+template<Periph usart, class Pin> constexpr Periph USART::stream()
 {
    if constexpr (usart == Periph::USART1) {
       if      constexpr (std::is_same_v<Pin, PA9>  or std::is_same_v<Pin, PB6>) return Periph::DMA1_stream4; 
@@ -188,8 +194,8 @@ template<Periph usart, class Pin> constexpr Periph USART_<R>::stream()
    return Periph::DMA1_stream1;
 }
 
-template <class R>
-constexpr IRQn_Type USART_<R>::IRQn(Periph usart)
+
+constexpr IRQn_Type USART::IRQn(Periph usart)
 {
    return usart == Periph::USART1 ? USART1_IRQn :
           usart == Periph::USART2 ? USART2_IRQn :
@@ -197,8 +203,8 @@ constexpr IRQn_Type USART_<R>::IRQn(Periph usart)
           NonMaskableInt_IRQn;
 }
 
-template <class R>
-template<Periph usart, class TXpin, class RXpin> void USART_<R>::pin_static_assert()
+
+template<Periph usart, class TXpin, class RXpin> void USART::pin_static_assert()
 {
    if constexpr (usart == Periph::USART1) {
          static_assert (
---
+++ test/f1_test_periph_usart.cpp
@@ -0,0 +1,425 @@
+#define BOOST_TEST_MODULE f1_test_periph_usart
+#include <boost/test/unit_test.hpp>
+
+#define F_CPU   72'000'000UL
+#define STM32F103xB
+#define TEST
+
+#include <type_traits>
+#include "mock_rcc.h"
+#include "mock_afio.h"
+#include "mock_gpio.h"
+#include "periph_usart.h"
+#include "timeout.h"
+#include "literals.h"
+
+BOOST_AUTO_TEST_SUITE (test_suite_main)
+
+
+mcu::USART usart;
+auto& cmsis = usart.like_CMSIS();
+
+BOOST_AUTO_TEST_CASE (like_CMSIS)
+{
+   auto same = std::is_same_v<std::remove_reference_t<decltype(cmsis)>, USART_TypeDef>;
+   auto address_usart = reinterpret_cast<size_t>(&usart);
+   auto address_CMSIS = reinterpret_cast<size_t>(&cmsis);
+   // BOOST_CHECK_EQUAL (address_usart, address_CMSIS);
+   BOOST_CHECK_EQUAL (same, true);
+   // BOOST_CHECK_EQUAL (sizeof(usart), sizeof(cmsis));
+}
+
+BOOST_AUTO_TEST_CASE (clear_interrupt_flags)
+{
+   struct mock : mcu::USART {
+      volatile bool SR_readed {false};
+      uint32_t read_SR() override { SR_readed = true; }
+      volatile bool DR_readed {false};
+      uint32_t read_DR() override { DR_readed = true; }
+   } usart;
+
+   auto worker = [&]() { usart.clear_interrupt_flags(); };
+   std::thread {worker}.detach();
+
+   Timeout timeout {100_ms};
+   while (not usart.SR_readed and not timeout) {}
+   BOOST_CHECK_EQUAL(bool(timeout), false);
+   BOOST_CHECK_EQUAL(usart.SR_readed, true);
+   BOOST_CHECK_EQUAL(usart.DR_readed, false);
+
+   while (not usart.DR_readed and not timeout) {}
+   BOOST_CHECK_EQUAL(bool(timeout), false);
+   BOOST_CHECK_EQUAL(usart.SR_readed, true);
+   BOOST_CHECK_EQUAL(usart.DR_readed, true);
+}
+
+BOOST_AUTO_TEST_CASE (set_parity)
+{
+   cmsis.CR1 = 0;
+   usart.set (mcu::USART::Parity::odd);
+   BOOST_CHECK_EQUAL (cmsis.CR1, USART_CR1_PS_Msk);
+
+   usart.set(mcu::USART::Parity::even);
+   BOOST_CHECK_EQUAL (cmsis.CR1, 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+/*
+
+
+bool parity()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.set(mcu::USART::Parity::odd);
+   good &= bool (CMSIS.CR1 & USART_CR1_PS_Msk);
+   usart.set(mcu::USART::Parity::even);
+   good &= not (CMSIS.CR1 & USART_CR1_PS_Msk);
+   return good;
+}
+
+bool wake_method()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.set(mcu::USART::WakeMethod::idle);
+   good &= not (CMSIS.CR1 & USART_CR1_WAKE_Msk);
+   usart.set(mcu::USART::WakeMethod::address);
+   good &= bool (CMSIS.CR1 & USART_CR1_WAKE_Msk);
+   return good;
+}
+
+bool data_bits()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.set(mcu::USART::DataBits::_9);
+   good &= bool (CMSIS.CR1 & USART_CR1_M_Msk);
+   usart.set(mcu::USART::DataBits::_8);
+   good &= not (CMSIS.CR1 & USART_CR1_M_Msk);
+   return good;
+}
+
+bool break_detection()
+{
+   CMSIS.CR2 = 0;
+   bool good {true};
+   usart.set(mcu::USART::BreakDetection::_11bit);
+   good &= bool (CMSIS.CR2 & USART_CR2_LBDL_Msk);
+   usart.set(mcu::USART::BreakDetection::_10bit);
+   good &= not (CMSIS.CR2 & USART_CR2_LBDIE_Msk);
+   return good;
+}
+
+bool stop_bits()
+{
+   CMSIS.CR2 = 0;
+   bool good {true};
+   usart.set(mcu::USART::StopBits::_1_5);
+   good &= bool (CMSIS.CR2 & USART_CR2_STOP_0)
+       and      (CMSIS.CR2 & USART_CR2_STOP_1);
+   usart.set(mcu::USART::StopBits::_2);
+   good &= not  (CMSIS.CR2 & USART_CR2_STOP_0)
+       and bool (CMSIS.CR2 & USART_CR2_STOP_1);
+   usart.set(mcu::USART::StopBits::_0_5);
+   good &= bool (CMSIS.CR2 & USART_CR2_STOP_0)
+       and not  (CMSIS.CR2 & USART_CR2_STOP_1);
+   usart.set(mcu::USART::StopBits::_1);
+   good &= not  (CMSIS.CR2 & USART_CR2_STOP_0)
+       and not  (CMSIS.CR2 & USART_CR2_STOP_1);
+   return good;
+}
+
+bool send_byte()
+{
+   CMSIS.DR = 0;
+   bool good {true};
+   usart.send_byte(0xFF);
+   good &= bool (CMSIS.DR & USART_DR_DR_Msk);
+   usart.send_byte(100);
+   good &= bool (CMSIS.DR & USART_DR_DR_Msk);
+   return good;
+}
+
+bool baudrate()
+{
+   CMSIS.BRR = 0;
+   bool good {true};
+   usart.set(mcu::USART::Baudrate::BR115200, 10000000);
+   good &= bool (CMSIS.BRR & USART_BRR_DIV_Mantissa_Msk);
+   usart.set(mcu::USART::Baudrate::BR57600, 20000000);
+   good &= bool (CMSIS.BRR & USART_BRR_DIV_Mantissa_Msk);
+   return good;
+}
+
+bool clock_enable()
+{
+   bool good {true};
+   usart.clock_enable<mcu::Periph::USART1, mcu::Periph::TEST_RCC>();
+   good &= mockRcc.good;
+   return good;
+}
+
+bool enable()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.enable();
+   good &= bool (CMSIS.CR1 & USART_CR1_UE_Msk);
+   return good;
+}
+
+bool disable()
+{
+   bool good {true};
+   usart.disable();
+   good &= not (CMSIS.CR1 & USART_CR1_UE_Msk);
+   return good;
+}
+
+bool rx_enable()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.rx_enable();
+   good &= bool (CMSIS.CR1 & USART_CR1_RE_Msk);
+   return good;
+}
+
+bool rx_disable()
+{
+   bool good {true};
+   usart.rx_disable();
+   good &= not (CMSIS.CR1 & USART_CR1_RE_Msk);
+   return good;
+}
+
+bool tx_enable()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.tx_enable();
+   good &= bool (CMSIS.CR1 & USART_CR1_TE_Msk);
+   return good;
+}
+
+bool tx_disable()
+{
+   bool good {true};
+   usart.tx_disable();
+   good &= not (CMSIS.CR1 & USART_CR1_TE_Msk);
+   return good;
+}
+
+bool rts_enable()
+{
+   CMSIS.CR3 = 0;
+   bool good {true};
+   usart.rts_enable();
+   good &= bool (CMSIS.CR3 & USART_CR3_RTSE_Msk);
+   return good;
+}
+
+bool rts_disable()
+{
+   bool good {true};
+   usart.rts_disable();
+   good &= not (CMSIS.CR3 & USART_CR3_RTSE_Msk);
+   return good;
+}
+
+bool DMA_tx_enable()
+{
+   CMSIS.CR3 = 0;
+   bool good {true};
+   usart.DMA_tx_enable();
+   good &= bool (CMSIS.CR3 & USART_CR3_DMAT_Msk);
+   return good;
+}
+
+bool DMA_rx_enable()
+{
+   CMSIS.CR3 = 0;
+   bool good {true};
+   usart.DMA_rx_enable();
+   good &= bool (CMSIS.CR3 & USART_CR3_DMAR_Msk);
+   return good;
+}
+
+bool parity_enable()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.parity_enable();
+   good &= bool (CMSIS.CR1 & USART_CR1_PCE_Msk);
+   return good;
+}
+
+bool parity_disable()
+{
+   bool good {true};
+   usart.parity_disable();
+   good &= not (CMSIS.CR1 & USART_CR1_PCE_Msk);
+   return good;
+}
+
+bool enable_IDLE_interrupt()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.enable_IDLE_interrupt();
+   good &= bool (CMSIS.CR1 & USART_CR1_IDLEIE_Msk);
+   return good;
+}
+
+bool is_IDLE_interrupt()
+{
+   CMSIS.SR = 0;
+   bool good {true};
+   good &= not usart.is_IDLE_interrupt();
+   CMSIS.SR |= USART_SR_IDLE;
+   good &= usart.is_IDLE_interrupt();
+   return good;
+}
+
+bool en_complete_interrupt()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   usart.enable_tx_complete_interrupt();
+   good &= bool (CMSIS.CR1 & USART_CR1_TCIE_Msk);
+   return good;
+}
+
+bool dis_complete_interrupt()
+{
+   bool good {true};
+   usart.disable_tx_complete_interrupt();
+   good &= not (CMSIS.CR1 & USART_CR1_TCIE_Msk);
+   return good;
+}
+
+bool is_tx_complete()
+{
+   CMSIS.SR = 0;
+   bool good {true};
+   good &= not usart.is_tx_complete();
+   CMSIS.SR |= USART_SR_TC;
+   good &= usart.is_tx_complete();
+   return good;
+}
+
+bool is_en_complete_interrupt()
+{
+   CMSIS.CR1 = 0;
+   bool good {true};
+   good &= not usart.is_tx_complete_interrupt_enable();
+   usart.enable_tx_complete_interrupt();
+   good &= usart.is_tx_complete_interrupt_enable();
+   return good;
+}
+
+bool receive_data_adr()
+{
+   bool good{true};
+   good &= bool ((reinterpret_cast<size_t>(&usart) + offsetof(mcu::USART::CMSIS_type, DR)) == usart.receive_data_adr());
+   return good;
+}
+
+bool transmit_data_adr()
+{
+   bool good{true};
+   good &= bool ((reinterpret_cast<size_t>(&usart) + offsetof(mcu::USART::CMSIS_type, DR)) == usart.receive_data_adr());
+   return good;
+}
+
+// bool DMA_channel()
+// {
+//    bool good {true};
+//    good &=  bool (mcu::DMA_bits::Channel::_4 == usart.DMA_channel(mcu::PinMode::USART1_TX))
+//         and bool (mcu::DMA_bits::Channel::_5 == usart.DMA_channel(mcu::PinMode::USART1_RX))
+//         and bool (mcu::DMA_bits::Channel::_7 == usart.DMA_channel(mcu::PinMode::USART2_TX))
+//         and bool (mcu::DMA_bits::Channel::_6 == usart.DMA_channel(mcu::PinMode::USART2_RX))
+//         and bool (mcu::DMA_bits::Channel::_2 == usart.DMA_channel(mcu::PinMode::USART3_TX))
+//         and bool (mcu::DMA_bits::Channel::_3 == usart.DMA_channel(mcu::PinMode::USART3_RX));
+//    return good;
+// }
+
+bool IRQn()
+{
+   bool good {true};
+   good &= bool (USART1_IRQn == usart.IRQn(mcu::Periph::USART1))
+       and not  (USART2_IRQn == usart.IRQn(mcu::Periph::USART1))
+       and bool (USART2_IRQn == usart.IRQn(mcu::Periph::USART2))
+       and not  (USART1_IRQn == usart.IRQn(mcu::Periph::USART2))
+       and bool (USART3_IRQn == usart.IRQn(mcu::Periph::USART3))
+       and not  (USART1_IRQn == usart.IRQn(mcu::Periph::USART3));
+   return good;
+}
+
+bool number_clock()
+{
+   bool good {true};
+   usart.clock<mcu::Periph::USART1, mcu::Periph::TEST_RCC>();
+   good &=     mockRcc.APB2;
+   good &= not mockRcc.APB1;
+   mockRcc.reset_APB();
+
+   usart.clock<mcu::Periph::USART2, mcu::Periph::TEST_RCC>();
+   good &=     mockRcc.APB1;
+   good &= not mockRcc.APB2;
+   mockRcc.reset_APB();
+
+   usart.clock<mcu::Periph::USART3, mcu::Periph::TEST_RCC>();
+   good &=     mockRcc.APB1;
+   good &= not mockRcc.APB2;
+   mockRcc.reset_APB();
+   return good;
+}
+
+int main()
+{
+   std::cout << '\n'
+             << "Тесты класса USART для STM32F0:" << std::endl;
+
+   auto test = [](auto s, auto f){
+      std::cout << s << (f() ? "\033[32mпрошёл\033[0m" : "\033[31mпровален\033[0m") << std::endl;
+   };
+
+   test ("USART::make_reference           ", make_reference_);
+   test ("USART::parity                   ", parity);
+   test ("USART::wake_method              ", wake_method);
+   test ("USART::data_bits                ", data_bits);
+   test ("USART::break_detection          ", break_detection);
+   test ("USART::stop_bits                ", stop_bits);
+   test ("USART::send_byte                ", send_byte);
+   test ("USART::baudrate                 ", baudrate);
+   test ("USART::clock_enable             ", clock_enable);
+   test ("USART::enable                   ", enable);
+   test ("USART::disable                  ", disable);
+   test ("USART::rx_enable                ", rx_enable);
+   test ("USART::rx_disable               ", rx_disable);
+   test ("USART::tx_enable                ", tx_enable);
+   test ("USART::tx_disable               ", tx_disable);
+   test ("USART::rts_enable               ", rts_enable);
+   test ("USART::rts_disable              ", rts_disable);
+   test ("USART::DMA_tx_enable            ", DMA_tx_enable);
+   test ("USART::DMA_rx_enable            ", DMA_rx_enable);
+   test ("USART::parity_enable            ", parity_enable);
+   test ("USART::parity_disable           ", parity_disable);
+   test ("USART::enable_IDLE_interrupt    ", enable_IDLE_interrupt);
+   test ("USART::is_IDLE_interrupt        ", is_IDLE_interrupt);
+   test ("USART::en_complete_interrupt    ", en_complete_interrupt);
+   test ("USART::dis_complete_interrupt   ", dis_complete_interrupt);
+   test ("USART::is_tx_complete           ", is_tx_complete);
+   test ("USART::is_en_complete_interrupt ", is_en_complete_interrupt);
+   test ("USART::clear_interrupt_flags    ", clear_interrupt_flags);
+   test ("USART::receive_data_adr         ", receive_data_adr);
+   test ("USART::transmit_data_adr        ", transmit_data_adr);
+//    test ("USART::DMA_channel              ", DMA_channel);
+   test ("USART::IRQn                     ", IRQn);
+   test ("USART::number_clock             ", number_clock);
+
+}
+*/
