d9417cafd6927114318478a7a7a996bd0360cd2a
Александр Александрович Голубев <aag@ganymed.factory.alexplus.ru>
2018-12-29 16:23:54 +0300

    line, cursor, central for lcd

--- src/lcd.h
+++ src/lcd.h
@@ -9,6 +9,12 @@
 
 namespace mcu {
 
+template<class T, class ... Args>
+constexpr bool all_is_same(T& t, Args& ... args)
+{
+    return ((t == args) and ...);
+}
+
 struct bit_set {
    size_t value;
    constexpr bit_set (size_t value) : value{value} {}
@@ -48,9 +54,10 @@ class LCD : TickSubscriber
 {
    using BSRR = std::pair<uint32_t, uint32_t>;
    
+   size_t line     {0};
    size_t index    {0};
-   size_t line     {1};
    size_t position {0};
+   bool centre {false};
    
    std::array<char, 80> screen;
 
@@ -84,7 +91,7 @@ class LCD : TickSubscriber
       delay<100>();
    }
 
-   void instruction (uint16_t command)
+   void instruction (uint32_t command)
    {
       rs = false;
       port.atomic_write(chars[command].first);
@@ -99,7 +106,8 @@ public:
    template <class RS, class RW, class E, class DB4, class DB5, class DB6, class DB7>
    static auto& make()
    {
-      // static_assert
+      static_assert ((all_is_same(DB4::periph, DB5::periph, DB6::periph, DB7::periph)),
+                     "Пины для шины экрана должны быть на одном порту");
       
       static auto screen = LCD 
       {
@@ -123,31 +131,11 @@ public:
    }
 
    void notify() override;
-
-   LCD& operator<< (std::string_view string)
-   {
-      size_t rest_string = string.size();
-      
-      if (rest_string < screen_size - position) {
-         std::copy(string.begin(), string.end(), screen.begin() + position);
-         position += rest_string;
-      } else {
-         std::copy(string.begin(), string.begin() + (screen_size - position), screen.begin() + position);
-         rest_string -= (screen_size - position);
-         while (rest_string > screen_size) {
-            auto string_begin = string.begin() + (string.size() - rest_string);
-            auto string_end   = string_begin + screen_size;
-            std::copy(string_begin, string_end, screen.begin());
-            rest_string -= screen_size;
-         }
-         std::copy(string.begin() + (string.size() - rest_string), string.end(), screen.begin());
-         position += rest_string;
-      }
-      
-      return *this;
-   }
-
-
+   LCD& operator<< (std::string_view string);
+   LCD& operator<< (size_t number);
+   LCD& set_line  (size_t string);
+   LCD& set_cursor(size_t cursor);
+   LCD& central();
 };
 
 
@@ -181,6 +169,8 @@ void LCD::notify()
 {
    switch (step) {
       case _1:
+         // e = true;
+         // e = false;
          rs = true;
          rw = false;
          port.atomic_write(chars[screen[index]].first);
@@ -211,6 +201,90 @@ void LCD::notify()
       break;
    }
 }
+
+
+
+LCD& LCD::operator<< (std::string_view string)
+{
+   size_t rest_string = string.size();
+      
+   if (centre){
+      if (rest_string < 20) {
+         size_t pos = (20 - rest_string)/2;
+         std::copy(string.begin(), string.end(), screen.begin() + (line*20 + pos));
+      } else {
+         std::copy(string.begin() + (rest_string - 20)/2, string.begin() + (rest_string - 20)/2 + 20, screen.begin() + line*20);
+      }
+   } else {
+      if (rest_string < screen_size - position) {
+         std::copy(string.begin(), string.end(), screen.begin() + position);
+         position += rest_string;
+      } else {
+         std::copy(string.begin(), string.begin() + (screen_size - position), screen.begin() + position);
+         rest_string -= (screen_size - position);
+         while (rest_string > screen_size) {
+            auto string_begin = string.begin() + (string.size() - rest_string);
+            auto string_end   = string_begin + screen_size;
+            std::copy(string_begin, string_end, screen.begin());
+            rest_string -= screen_size;
+         }
+         std::copy(string.begin() + (string.size() - rest_string), string.end(), screen.begin());
+         position += rest_string;
+      }
+   }
+      
+   return *this;
+}
+
+LCD& LCD::operator<< (size_t number)
+{
+   char ch [10];
+   snprintf(ch, sizeof(ch), "%d", number);
+   // __itoa(number, ch, 10);
+   // std::string_view s (ch);
+   *this << ch;
+   return *this;
+}
+
+LCD& LCD::central ()
+{
+   centre = true;
+   return *this;
+}
+
+LCD& LCD::set_line(size_t string)
+{
+   line = string;
+   position = line * 20;
+   return *this;
+}
+
+LCD& LCD::set_cursor(size_t cursor)
+{
+   if      (line == 0) {
+      if (cursor < 80)
+         position = cursor;
+      else position = cursor - 80 - (cursor/80) * 80;
+   } else if (line == 1) {
+      if (cursor < 60)
+         position = cursor + 20;
+      else position = cursor - 60 - (cursor/80) * 80;
+   } else if (line == 2) {
+      if (cursor < 40)
+         position = cursor + 40;
+      else position = cursor - 40 - (cursor/80) * 80;
+   } else if (line == 3) {
+      if (cursor < 20)
+         position = cursor + 60;
+      else position = cursor - 20 - (cursor/80) * 80;
+   }
+   
+   // position = line == 1 ? cursor :
+   //            line == 2 ? cursor + 20 :
+   //            line == 3 ? cursor + 40 : cursor + 60;
+}
+
+
 
 
 } // namespace mcu
