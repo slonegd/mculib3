ce9bebf1ac8ce056f401c96783b6f2d43b09ba24
Александр Александрович Голубев <aag@ganymed.factory.alexplus.ru>
2019-03-18 10:54:08 +0300

    init_clock_f0

--- src/periph/init_clock.h
+++ src/periph/init_clock.h
@@ -3,7 +3,7 @@
 #include "periph.h"
 
 #if   defined(STM32F0)
-   #error init_clock for STM32F0 not realized
+   #include "init_clock_f0.h"
 #elif defined(STM32F1)
    #error init_clock for STM32F1 not realized
 #elif defined(STM32F4)
