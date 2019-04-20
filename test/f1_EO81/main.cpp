7cec74d495bad89c6a3c4caa497c210634569114
slonegd <slonkdv@mail.ru>
2019-04-20 08:33:38 +0300

    copy select screen to middleware part of lib, make example

--- test/f1_EO81/main.cpp
+++ test/f1_EO81/main.cpp
@@ -3,17 +3,25 @@
 #define F_CPU   72000000UL
 // #include <iostream>
 #include "init_clock.h"
-#include "example_hd44780.h"
+// #include "example_hd44780.h"
+#include "example_select_screen.h"
 
 /// эта функция вызывается первой в startup файле
 extern "C" void init_clock() { init_clock<F_OSC, F_CPU>(); }
 
 int main()
 {
-    // values dont need
-    auto _ = mcu::example::hd44780<
+    // // value dont need
+    // auto _ = mcu::example::hd44780<
+    //     EO81::RS , EO81::RW , EO81::E,
+    //     EO81::DB4, EO81::DB5, EO81::DB6, EO81::DB7
+    // >();
+
+    // value dont need
+    auto _ = mcu::example::select_screen<
         EO81::RS , EO81::RW , EO81::E,
-        EO81::DB4, EO81::DB5, EO81::DB6, EO81::DB7
+        EO81::DB4, EO81::DB5, EO81::DB6, EO81::DB7,
+        EO81::Up, EO81::Down
     >();
 
     while(1){
