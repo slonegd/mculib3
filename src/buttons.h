6aaacd802e4955c759d3b62ad4320f17d4cbb110
slonegd <slonkdv@mail.ru>
2019-04-28 11:42:15 +0300

    fix buttons

--- src/buttons.h
+++ src/buttons.h
@@ -3,7 +3,7 @@
 #include "timers.h"
 #include "literals.h"
 #if not defined (TEST)
-   #include "pin.h"
+    #include "pin.h"
 #endif
 #include <tuple>
 #include <algorithm>
@@ -12,176 +12,156 @@ namespace mcu {
 
 class Button : TickSubscriber
 {
-   Pin& pin;
+    Pin& pin;
 
-   int time {0};
-   bool short_press {false};
-   bool long_press {false};
-   bool increment  {false};
-   bool inverted   {false};
-   bool click_     {false};
-   int  inc {1};
-   int  value{0};
-   int  tmp{0};
+    int time {0};
+    bool short_press {false};
+    bool long_press {false};
+    bool increment  {false};
+    bool inverted   {false};
+    bool click_     {false};
+    int  inc {1};
+    int  value{0};
+    int  tmp{0};
 
-   Button (Pin& pin, bool inverted) : pin {pin}, inverted {inverted} {}
+    Button (Pin& pin, bool inverted) : pin {pin}, inverted {inverted} {}
 
 public:
 
-   template<class Pin_, bool inverted = false> 
-   static auto& make()
-   {
-      static auto button = Button {Pin::template make<Pin_, PinMode::Input>(), inverted};
-      return button;
-   }
+    template<class Pin_, bool inverted = false> 
+    static auto& make()
+    {
+        static auto button = Button {Pin::template make<Pin_, PinMode::Input>(), inverted};
+        return button;
+    }
 
-   bool push()
-   {
-      if (inverted) {
-         if (not pin) 
+    bool push()
+    {
+        if (inverted xor pin)
             tick_subscribe();
-      } else {
-         if (pin)
+
+        return time >= 10 and not short_press ? (short_press = true) : false;
+    }
+
+    bool push_long()
+    {
+        if (inverted xor pin)
             tick_subscribe();
-      }
 
-      return time >= 10 and not short_press ? (short_press = true) : false;
-   }
+        return time >= 1000 and not long_press ? (long_press = true) : false;
+    }
 
-   bool push_long()
-   {
-      if (inverted) {
-         if (not pin) 
-            tick_subscribe();
-      } else {
-         if (pin)
-            tick_subscribe();
-      }
+    // bool click()
+    // {
+    //    if (inverted) {
+    //       if (not pin) 
+    //          tick_subscribe();
+    //    } else {
+    //       if (pin)
+    //          tick_subscribe();
+    //    }
+    //    if (time > 10 and time < 1000)
+    //       click_ = true;
+    //    return not pin and click_ ? (not (click_ = false)) : false;
+    // }
 
-      return time >= 1000 and not long_press ? (long_press = true) : false;
-   }
+    operator int()
+    {
+        if (increment) {
+            if (time <= 2000)
+                value = (time >= tmp + 200) ? tmp = time, inc : 0;
+            else if (time < 3000)
+                value = (time >= tmp + 100) ? tmp = time, inc : 0;
+            else if (time < 4000)
+                value = (time >= tmp + 50 ) ? tmp = time, inc : 0;
+            else if (time < 5000)
+                value = (time >= tmp + 25 ) ? tmp = time, inc : 0;
+            else if (time >= 5000) {
+                value = (time >= tmp + 25 ) ? tmp = time, inc : 0;
+                inc += (time % 1000) == 0;
+            }
+            
+        } else if (push()) 
+            value = 1;
+         else if (push_long()) {
+            increment = true;
+            tmp = time;
+            value = 1;
+        } else 
+            value = 0;
 
-   // bool click()
-   // {
-   //    if (inverted) {
-   //       if (not pin) 
-   //          tick_subscribe();
-   //    } else {
-   //       if (pin)
-   //          tick_subscribe();
-   //    }
-   //    if (time > 10 and time < 1000)
-   //       click_ = true;
-   //    return not pin and click_ ? (not (click_ = false)) : false;
-   // }
+        return value;
+    }
 
-   operator int()
-   {
-      if (increment) {
-         if (time <= 2000)
-            value = (time >= tmp + 200) ? tmp = time, inc : 0;
-         else if (time < 3000)
-            value = (time >= tmp + 100) ? tmp = time, inc : 0;
-         else if (time < 4000)
-            value = (time >= tmp + 50 ) ? tmp = time, inc : 0;
-         else if (time < 5000)
-            value = (time >= tmp + 25 ) ? tmp = time, inc : 0;
-         else if (time >= 5000) {
-            value = (time >= tmp + 25 ) ? tmp = time, inc : 0;
-            inc += (time % 1000) == 0;
-         }
-         
-      } else if (push()) 
-         value = 1;
-       else if (push_long()) {
-         increment = true;
-         tmp = time;
-         value = 1;
-      } else 
-         value = 0;
-
-      return value;
-   }
-
-   void notify () override
-   {
-      if (inverted) {
-         if (pin) {
+    void notify () override
+    {
+        if (inverted xor pin) {
+            time++;
+        } else {
             tick_unsubscribe();
             time = 0;
             short_press = false;
             long_press = false;
             increment  = false;
             inc        = 1;
-         } else 
-            time++;
-      } else {
-         if (not pin) {
-            tick_unsubscribe();
-            time = 0;
-            short_press = false;
-            long_press = false;
-            increment  = false;
-            click_     = false;
-            inc        = 1;
-         } else 
-            time++;
-      }
-   }
+        }
+    }
 
-   bool is_set()
-   {
-      if (inverted) return not pin;
-      else return pin;
-   }
+    bool is_push() { return inverted xor pin; }
 
-   struct Some 
-   {
-      Button& one;
-      Button& two;
-      Some(Button& one, Button& two) : one{one}, two{two} {}
-      operator bool () {
-         if (one.is_set() and two.is_set()) {
-            one.tick_subscribe();
-            two.tick_subscribe(); 
-         }
-         bool result{false};
-         result = not one.pin and not two.pin and (not one.short_press or not two.short_press);
-         if (result)
-            one.short_press = two.short_press = true;
-         return result;
-      }                  
+    struct Proxy_buttons 
+    {
+        Button& one;
+        Button& two;
 
-      bool click()
-      {
-         if (one.is_set() and two.is_set()) {
-            one.tick_subscribe();
-            two.tick_subscribe(); 
-         }
-         bool result{false};
-         if (one.time > 100 and one.time < 200 and two.time > 100 and two.time < 200)
-            one.click_ = two.click_ = true;
-         result = not one.is_set() and not two.is_set() and (one.click_ or two.click_) and not  one.long_press and not two.long_press;
-         if (result)
-            one.click_ = two.click_ = false;
-         return result;
-      }                          
+        Proxy_buttons(Button& one, Button& two) : one{one}, two{two} {}
 
-      bool push_long() {
-         if (one.is_set() and two.is_set()) {
-            one.tick_subscribe();
-            two.tick_subscribe(); 
-         }
-         bool result{false};
-         result = one.time >= 1000 and two.time >= 1000 and (not one.long_press or not two.long_press);
-         if (result)
-            one.long_press = two.long_press = true;
-         return result;
-      }
-   };
+        operator bool () {
+            if (one.is_push()) one.tick_subscribe(); 
+            if (two.is_push()) two.tick_subscribe();
+            auto result =
+                    one.time >= 10
+                and two.time >= 10
+                and (not one.short_press
+                  or not two.short_press
+                );
+            if (result)
+                one.short_press = two.short_press = true;
+            return result;
+        }                  
 
-   Some operator and (Button& other) {return Some{*this, other};}
-   friend Some;
+        bool click()
+        {
+            if (one.is_push() and two.is_push()) {
+                one.tick_subscribe();
+                two.tick_subscribe(); 
+            }
+            bool result{false};
+            if (one.time > 100 and one.time < 200 and two.time > 100 and two.time < 200)
+                one.click_ = two.click_ = true;
+            result = not one.is_push() and not two.is_push() and (one.click_ or two.click_) and not  one.long_press and not two.long_press;
+            if (result)
+                one.click_ = two.click_ = false;
+            return result;
+        }                          
+
+        bool push_long() {
+            if (one.is_push()) one.tick_subscribe(); 
+            if (two.is_push()) two.tick_subscribe();
+            auto result =
+                    one.time >= 1000
+                and two.time >= 1000
+                and (not one.long_press
+                  or not two.long_press
+                );
+            if (result)
+                one.long_press = two.long_press = true;
+            return result;
+        }
+    };
+
+    Proxy_buttons operator and (Button& other) {return Proxy_buttons{*this, other};}
+    friend Proxy_buttons;
 
 };
 
--- test/test_button.cpp
+++ test/test_button.cpp
@@ -3,350 +3,348 @@
 
 #define F_CPU   72000000UL
 #define STM32F103xB
-#define TEST
 
-#include "mock_pin.h"
+#include "mock_gpio.h"
+#include "mock_systick.h"
 #include "buttons.h"
 
 BOOST_AUTO_TEST_SUITE (test_suite_main)
 
-enum button {up_ = 0, down_ = 1};
-
 template <class Function>
-void process (int n, Function f)
+void do_n_ticks (int n, Function f)
 {
-   while (n--) {
-      SysTick_Handler();
-      f();
-   }
+    while (n--) {
+        SysTick_Handler();
+        f();
+    }
 }
 
 auto passed_ms = [&](int n){while (n--) SysTick_Handler();};
 
-auto& up   = mcu::Button::make<mcu::PA1>();
-auto& down = mcu::Button::make<mcu::PA2, true>();
+constexpr auto inverted = true;
+
+auto& but     = mcu::Button::make<mcu::PA1>();
+auto& inv_but = mcu::Button::make<mcu::PA2, inverted>();
 
 BOOST_AUTO_TEST_CASE(push)
 {
-   mcu::pins[button::up_]->set = true;
-   process(10, [&](){
-      BOOST_CHECK_EQUAL(up.push(), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(up.push(), true); // return true only once
-   BOOST_CHECK_EQUAL(up.push(), false);
-   mcu::pins[button::up_]->set = false;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, true);
+    do_n_ticks (10, [&]{
+        BOOST_CHECK_EQUAL(but.push(), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(but.push(), true); // return true only once
+    BOOST_CHECK_EQUAL(but.push(), false);
+    REF(GPIOA).mock.set (1, false);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(push_long)
 {
-   mcu::pins[button::up_]->set = true;
-   process(1000, [&](){
-      BOOST_CHECK_EQUAL(up.push_long(), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(up.push_long(), true); // return true only once
-   BOOST_CHECK_EQUAL(up.push_long(), false);
-   mcu::pins[button::up_]->set = false;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, true);
+    do_n_ticks (1000, [&]{
+        BOOST_CHECK_EQUAL(but.push_long(), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(but.push_long(), true); // return true only once
+    BOOST_CHECK_EQUAL(but.push_long(), false);
+    REF(GPIOA).mock.set (1, false);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(increment)
 {
-   int var = 0;
-   mcu::pins[button::up_]->set = true;
-   process(10, [&](){
-      var += up;
-      BOOST_CHECK_EQUAL(var, 0);
-   });
-   process(1, [&](){
-      var += up;
-      BOOST_CHECK_EQUAL(var, 1);
-   });
+    int var = 0;
+    REF(GPIOA).mock.set (1, true);
+    do_n_ticks (10, [&]{
+        var += but;
+        BOOST_CHECK_EQUAL(var, 0);
+    });
+    do_n_ticks (1, [&]{
+        var += but;
+        BOOST_CHECK_EQUAL(var, 1);
+    });
 
-   process(989, [&](){
-      var += up;
-      BOOST_CHECK_EQUAL(var, 1);
-   });
-   process(1, [&](){
-      var += up;
-      BOOST_CHECK_EQUAL(var, 2);
-   });
+    do_n_ticks (989, [&]{
+        var += but;
+        BOOST_CHECK_EQUAL(var, 1);
+    });
+    do_n_ticks (1, [&]{
+        var += but;
+        BOOST_CHECK_EQUAL(var, 2);
+    });
 
-   process(200, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 3);
+    do_n_ticks (200, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 3);
 
-   process(200, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 4);
+    do_n_ticks (200, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 4);
 
-   process(600, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 7);
+    do_n_ticks (600, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 7);
 
-   process(100, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 8);
+    do_n_ticks (100, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 8);
 
-   process(200, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 10);
+    do_n_ticks (200, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 10);
 
-   process(700, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 17);
+    do_n_ticks (700, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 17);
 
-   process(50, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 18);
+    do_n_ticks (50, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 18);
 
-   process(950, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 37);
+    do_n_ticks (950, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 37);
 
-   process(25, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 38);
+    do_n_ticks (25, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 38);
 
-   process(975, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 77);
+    do_n_ticks (975, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 77);
 
-   process(25, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 79);
+    do_n_ticks (25, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 79);
 
-   process(25, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 81);
+    do_n_ticks (25, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 81);
 
-   process(25, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 83);
+    do_n_ticks (25, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 83);
 
-   process(25, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 85);
+    do_n_ticks (25, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 85);
 
-   process(900, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 157);
+    do_n_ticks (900, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 157);
 
-   process(1000, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 277);
+    do_n_ticks (1000, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 277);
 
-   process(1000, [&](){var += up;});
-   BOOST_CHECK_EQUAL(var, 437);
+    do_n_ticks (1000, [&]{var += but;});
+    BOOST_CHECK_EQUAL(var, 437);
 
 
-   mcu::pins[button::up_]->set = false;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, false);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(push_inverted)
 {
-   mcu::pins[button::down_]->set = false;
-   process(10, [&](){
-      BOOST_CHECK_EQUAL(down.push(), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(down.push(), true); // return true only once
-   BOOST_CHECK_EQUAL(down.push(), false);
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (2, false);
+    do_n_ticks (10, [&]{
+        BOOST_CHECK_EQUAL(inv_but.push(), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(inv_but.push(), true); // return true only once
+    BOOST_CHECK_EQUAL(inv_but.push(), false);
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(push_long_inverted)
 {
-   mcu::pins[button::down_]->set = false;
-   process(1000, [&](){
-      BOOST_CHECK_EQUAL(down.push_long(), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(down.push_long(), true); // return true only once
-   BOOST_CHECK_EQUAL(down.push_long(), false);
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (2, false);
+    do_n_ticks (1000, [&]{
+        BOOST_CHECK_EQUAL(inv_but.push_long(), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(inv_but.push_long(), true); // return true only once
+    BOOST_CHECK_EQUAL(inv_but.push_long(), false);
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(decrement)
 {
-   int var = 437;
+    int var = 437;
 
-   mcu::pins[button::down_]->set = false;
-   down.push();
-   process(10, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 436);
+    REF(GPIOA).mock.set (2, false);
+    inv_but.push();
+    do_n_ticks (10, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 436);
 
-   process(1000, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 435);
+    do_n_ticks (1000, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 435);
 
-   process(200, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 434);
+    do_n_ticks (200, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 434);
 
-   process(200, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 433);
+    do_n_ticks (200, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 433);
 
-   process(600, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 430);
+    do_n_ticks (600, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 430);
 
-   process(100, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 429);
+    do_n_ticks (100, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 429);
 
-   process(200, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 427);
+    do_n_ticks (200, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 427);
 
-   process(700, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 420);
+    do_n_ticks (700, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 420);
 
-   process(50, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 419);
+    do_n_ticks (50, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 419);
 
-   process(950, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 400);
+    do_n_ticks (950, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 400);
 
-   process(25, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 399);
+    do_n_ticks (25, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 399);
 
-   process(975, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 360);
+    do_n_ticks (975, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 360);
 
-   process(25, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 358);
+    do_n_ticks (25, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 358);
 
-   process(25, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 356);
+    do_n_ticks (25, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 356);
 
-   process(25, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 354);
+    do_n_ticks (25, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 354);
 
-   process(25, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 352);
+    do_n_ticks (25, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 352);
 
-   process(900, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 280);
+    do_n_ticks (900, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 280);
 
-   process(1000, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 160);
+    do_n_ticks (1000, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 160);
 
-   process(1000, [&](){var -= down;});
-   BOOST_CHECK_EQUAL(var, 0);
+    do_n_ticks (1000, [&]{var -= inv_but;});
+    BOOST_CHECK_EQUAL(var, 0);
 
-
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(both_together_push)
 {
-   mcu::pins[button::up_]->set = true;
-   mcu::pins[button::down_]->set = false;
-   process(10, [&](){
-      BOOST_CHECK_EQUAL(bool (up and down), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(bool (up and down), true);
-   BOOST_CHECK_EQUAL(bool (up and down), false);
+    REF(GPIOA).mock.set (1, true);
+    REF(GPIOA).mock.set (2, false);
+    do_n_ticks (10, [&]{
+        BOOST_CHECK_EQUAL(bool (but and inv_but), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(bool (but and inv_but), true);
+    BOOST_CHECK_EQUAL(bool (but and inv_but), false);
 
-   mcu::pins[button::up_]->set = false;
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, false);
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(both_together_push_long)
 {
-   mcu::pins[button::up_]->set = true;
-   mcu::pins[button::down_]->set = false;
-   process(1000, [&](){
-      BOOST_CHECK_EQUAL(bool ((up and down).push_long()), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(bool ((up and down).push_long()), true);
+    REF(GPIOA).mock.set (1, true);
+    REF(GPIOA).mock.set (2, false);
+    do_n_ticks (1000, [&]{
+        BOOST_CHECK_EQUAL(bool ((but and inv_but).push_long()), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(bool ((but and inv_but).push_long()), true);
 
-   mcu::pins[button::up_]->set = false;
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, false);
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(both_push)
 {
-   mcu::pins[button::up_]->set = true;
-   process(5, [&](){
-      BOOST_CHECK_EQUAL(bool (up and down), false);
-   });
+    REF(GPIOA).mock.set (1, true);
+    do_n_ticks (5, [&]{
+        BOOST_CHECK_EQUAL(bool (but and inv_but), false);
+    });
 
-   mcu::pins[button::down_]->set = false;
-   process(5, [&](){
-      BOOST_CHECK_EQUAL(bool (up and down), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(bool (up and down), false);
-   process(4, [&](){
-      BOOST_CHECK_EQUAL(bool (up and down), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(bool (up and down), true);
-   BOOST_CHECK_EQUAL(bool (up and down), false);
+    REF(GPIOA).mock.set (2, false);
+    do_n_ticks (5, [&]{
+        BOOST_CHECK_EQUAL(bool (but and inv_but), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(bool (but and inv_but), false);
+    do_n_ticks (4, [&]{
+        BOOST_CHECK_EQUAL(bool (but and inv_but), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(bool (but and inv_but), true);
+    BOOST_CHECK_EQUAL(bool (but and inv_but), false);
 
-   mcu::pins[button::up_]->set = false;
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, false);
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(both_push_long)
 {
-   mcu::pins[button::up_]->set = true;
-   process(100, [&](){
-      BOOST_CHECK_EQUAL(bool ((up and down).push_long()), false);
-   });
-   
-   mcu::pins[button::down_]->set = false;
-   process(900, [&](){
-      BOOST_CHECK_EQUAL(bool ((up and down).push_long()), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(bool ((up and down).push_long()), false);
-   process(99, [&](){
-      BOOST_CHECK_EQUAL(bool ((up and down).push_long()), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(bool ((up and down).push_long()), true);
-   BOOST_CHECK_EQUAL(bool ((up and down).push_long()), false);
+    REF(GPIOA).mock.set (1, true);
+    do_n_ticks (100, [&]{
+        BOOST_CHECK_EQUAL(bool ((but and inv_but).push_long()), false);
+    });
+    
+    REF(GPIOA).mock.set (2, false);
+    do_n_ticks (900, [&]{
+        BOOST_CHECK_EQUAL(bool ((but and inv_but).push_long()), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(bool ((but and inv_but).push_long()), false);
+    do_n_ticks (99, [&]{
+        BOOST_CHECK_EQUAL(bool ((but and inv_but).push_long()), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(bool ((but and inv_but).push_long()), true);
+    BOOST_CHECK_EQUAL(bool ((but and inv_but).push_long()), false);
 
-   mcu::pins[button::up_]->set = false;
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, false);
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(both_push_after_push)
 {
-   mcu::pins[button::up_]->set = true;
-   process(10, [&](){
-      BOOST_CHECK_EQUAL(up.push(), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(up.push(), true);
-   BOOST_CHECK_EQUAL(up.push(), false);
+    REF(GPIOA).mock.set (1, true);
+    do_n_ticks (10, [&]{
+        BOOST_CHECK_EQUAL(but.push(), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(but.push(), true);
+    BOOST_CHECK_EQUAL(but.push(), false);
 
-   mcu::pins[button::down_]->set = false;
-   process(10, [&](){
-      BOOST_CHECK_EQUAL(bool (up and down), false);
-   });
+    REF(GPIOA).mock.set (2, false);
+    do_n_ticks (10, [&]{
+        BOOST_CHECK_EQUAL(bool (but and inv_but), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(bool (but and inv_but), true);
+    BOOST_CHECK_EQUAL(bool (but and inv_but), false);
 
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(bool (up and down), true);
-   BOOST_CHECK_EQUAL(bool (up and down), false);
-
-   mcu::pins[button::up_]->set = false;
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, false);
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 BOOST_AUTO_TEST_CASE(both_not_push)
 {
-   mcu::pins[button::up_]->set = true;
-   mcu::pins[button::down_]->set = false;
-   process(5, [&](){
-      BOOST_CHECK_EQUAL(bool (up and down), false);
-   });
+    REF(GPIOA).mock.set (1, true);
+    REF(GPIOA).mock.set (2, false);
+    do_n_ticks (5, [&]{
+        BOOST_CHECK_EQUAL(bool (but and inv_but), false);
+    });
 
-   mcu::pins[button::down_]->set = true;
-   process(5, [&](){
-      BOOST_CHECK_EQUAL(bool (up and down), false);
-   });
-   passed_ms(1);
-   BOOST_CHECK_EQUAL(bool (up and down), false);
+    REF(GPIOA).mock.set (2, true);
+    do_n_ticks (5, [&]{
+        BOOST_CHECK_EQUAL(bool (but and inv_but), false);
+    });
+    passed_ms(1);
+    BOOST_CHECK_EQUAL(bool (but and inv_but), false);
 
-   mcu::pins[button::up_]->set = false;
-   mcu::pins[button::down_]->set = true;
-   passed_ms(1); // unsubscribe
+    REF(GPIOA).mock.set (1, false);
+    REF(GPIOA).mock.set (2, true);
+    passed_ms(1); // unsubscribe
 }
 
 
