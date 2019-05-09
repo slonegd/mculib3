d96c5c125e2ac7b8ddc922773d7d0ada07cd2f2f
slonegd <slonkdv@mail.ru>
2019-05-09 09:15:49 +0300

    fix string buffer bug with next_line + add test for case

--- src/string_buffer.h
+++ src/string_buffer.h
@@ -6,10 +6,12 @@
 
 class String_buffer
 {
-    size_t  number_line{0};
-    size_t  width_size {0};
     size_t  position   {0};
-    bool    align  {false};
+    // только для пользовательского форматирования следующие переменные
+    // !!! не использовать как инфу о состоянии буфера
+    // size_t  number_line{0};
+    size_t  width_size {0};
+    bool    align_center  {false};
 
     std::array<char, 80> screen;
     static constexpr size_t line_size = 20;
@@ -22,6 +24,7 @@ public:
     String_buffer (){screen.fill(' ');}
     String_buffer& operator<< (std::string_view string);
     String_buffer& operator<< (int number);
+    String_buffer& operator<< (char);
     String_buffer& operator<< (String_buffer_ref& function);
     String_buffer& line   (size_t string);
     String_buffer& width  (size_t width );
@@ -31,29 +34,33 @@ public:
     auto begin        () const {return screen.begin();}
     auto end          () const {return screen.end()  ;}
     auto get_position () const {return position;}
-    auto get_line     () const {return number_line;}
-    void clear        (){screen.fill(' ');}
+    auto get_line     () const {return position / line_size;}
+    void clear        (){screen.fill(' '); position = 0; }
     bool in_begin_line() const { return position % line_size == 0; }
 
 };
 
-// FIX зависает на 79 позиции
 String_buffer& next_line (String_buffer& s) 
 {
     while (not s.in_begin_line())
-        s << " ";
+        s << ' ';
     return s;
+}
+
+String_buffer& String_buffer::operator<< (char v)
+{
+    *(screen.begin() + position++) = v;
+    return *this;
 }
 
 
 String_buffer& String_buffer::operator<< (std::string_view string)
 {
     size_t string_size = string.size();
-    auto begin_line    = screen.begin() + number_line * line_size;
-    auto end_line      = screen.begin() + number_line * line_size + line_size;
+    auto begin_line    = screen.begin() + position;
+    auto end_line      = screen.begin() + position + line_size;
 
-    // if (width_) {position += (width_size - string_size); width_ = false;}
-    if (align){
+    if (align_center){
         if (string_size < line_size) {
             auto begin_string = begin_line + (line_size - string_size)/2;
             auto end_string   = begin_string + string.size();
@@ -61,15 +68,16 @@ String_buffer& String_buffer::operator<< (std::string_view string)
             std::fill(begin_line    , begin_string, ' ');
             std::copy(string.begin(), string.end(), begin_string);
             std::fill(end_string    , end_line    , ' ');
-            align = false;
+            align_center = false;
         } else {
             auto begin_string = string.begin() + (string_size - line_size)/2;
             auto end_string   = begin_string + line_size;
 
             std::copy(begin_string, end_string, begin_line);
-            align = false;
+            align_center = false;
         }
-        position = number_line * line_size + line_size;
+        position += line_size;
+        position %= screen_size;
     } else {
         if (string_size < screen_size - position) {
             std::copy(string.begin(), string.end(), screen.begin() + position);
@@ -86,8 +94,11 @@ String_buffer& String_buffer::operator<< (std::string_view string)
             }
             std::copy(string.begin() + (string_size - rest_string), string.end(), screen.begin());
             position += rest_string;
+            position %= screen_size;
         }
     }
+
+    align_center = false;
     
     return *this;
 }
@@ -99,9 +110,8 @@ String_buffer& String_buffer::operator<< (int number)
     std::string_view str (string);
     auto qty_space = (width_size > str.size()) ? (width_size - str.size()) : 0;
 
-    while (qty_space) {
-        *this << " ";
-        qty_space--;
+    while (qty_space--) {
+        *this << ' ';
     }
     *this << string;
     width_size = 0;
@@ -113,10 +123,9 @@ String_buffer& String_buffer::operator<< (String_buffer_ref& function)
     return function(*this);
 }
 
-String_buffer& String_buffer::line(size_t string)
+String_buffer& String_buffer::line(size_t v)
 {
-    number_line = string;
-    position = number_line * line_size;
+    position = v * line_size;
     return *this;
 }
 
@@ -129,12 +138,12 @@ String_buffer& String_buffer::width(size_t width)
 
 String_buffer& String_buffer::cursor(size_t cursor)
 {
-    position = (cursor + number_line * line_size) % screen_size;
+    position = (cursor + get_line() * line_size) % screen_size;
     return *this;
 }
 
 String_buffer& String_buffer::center ()
 {
-    align = true;
+    align_center = true;
     return *this;
 }
