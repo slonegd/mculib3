e3f9a8bcc95741c837521b915c52e3b0f6c6e07c
Денис Владимирович Кирюшин <dvk@phobos.factory.alexplus.ru>
2019-06-01 10:57:45 +0300

    fix f1: interrupt flags dma

--- src/periph/dma_f1.h
+++ src/periph/dma_f1.h
@@ -39,25 +39,30 @@ template <Periph p> std::enable_if_t<p == Periph::DMA1, DMA&> make_reference() {
 
 void DMA::clear_interrupt_flags(Channel channel)
 {
-   if      (channel == Channel::_1) IFCR.CGIF1 = true;
-   else if (channel == Channel::_2) IFCR.CGIF2 = true;
-   else if (channel == Channel::_3) IFCR.CGIF3 = true;
-   else if (channel == Channel::_4) IFCR.CGIF4 = true;
-   else if (channel == Channel::_5) IFCR.CGIF5 = true;
-   else if (channel == Channel::_6) IFCR.CGIF6 = true;
-   else if (channel == Channel::_7) IFCR.CGIF7 = true;
+   // -fno-strict-volatile-bitfields dosent work
+   // if      (channel == Channel::_1) IFCR.CGIF1 = true;
+   // else if (channel == Channel::_2) IFCR.CGIF2 = true;
+   // else if (channel == Channel::_3) IFCR.CGIF3 = true;
+   // else if (channel == Channel::_4) IFCR.CGIF4 = true;
+   // else if (channel == Channel::_5) IFCR.CGIF5 = true;
+   // else if (channel == Channel::_6) IFCR.CGIF6 = true;
+   // else if (channel == Channel::_7) IFCR.CGIF7 = true;
+   registr(IFCR) = (1 << ((static_cast<uint8_t>(channel) - 1)*4));
 }
 
 bool DMA::is_transfer_complete_interrupt(Channel channel)
 {
-   if      (channel == Channel::_1) return ISR.TCIF1;
-   else if (channel == Channel::_2) return ISR.TCIF2;
-   else if (channel == Channel::_3) return ISR.TCIF3;
-   else if (channel == Channel::_4) return ISR.TCIF4;
-   else if (channel == Channel::_5) return ISR.TCIF5;
-   else if (channel == Channel::_6) return ISR.TCIF6;
-   else if (channel == Channel::_7) return ISR.TCIF7;
-   else return false;
+   // -fno-strict-volatile-bitfields dosent work
+   // if      (channel == Channel::_1) return ISR.TCIF1;
+   // else if (channel == Channel::_2) return ISR.TCIF2;
+   // else if (channel == Channel::_3) return ISR.TCIF3;
+   // else if (channel == Channel::_4) return ISR.TCIF4;
+   // else if (channel == Channel::_5) return ISR.TCIF5;
+   // else if (channel == Channel::_6) return ISR.TCIF6;
+   // else if (channel == Channel::_7) return ISR.TCIF7;
+   // else return false;
+   return registr(ISR) & (1 << (1 + (static_cast<uint8_t>(channel) - 1)*4));
+
 }
 
 } //namespace mcu
