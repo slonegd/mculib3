67846e4fe9bcc9fd5629b0b72625da5615095563
Денис Владимирович Кирюшин <dvk@phobos.factory.alexplus.ru>
2019-08-10 09:17:40 +0300

    уточнение для аргументов функции

--- src/utils.h
+++ src/utils.h
@@ -1,8 +1,11 @@
 #pragma once
 
+// указатель для ясности: какой из аргументов меняется
+// set_if_greater(&a, b)
+//                 ^--этот
 template <class T>
-void set_if_greater (T& what, T value)
+void set_if_greater (T* what, const T value)
 {
-    if (value > what)
-        what = value;
+    if (value > *what)
+        *what = value;
 }
